e5610c7e48df60b1db080947757d80b6aab80005
Izabella Williams <iza.yy10@gmail.com>
2022-03-29 15:58:36 -0400

    weather card

--- Objects/Structures/Cards/WeatherCard.cs
+++ Objects/Structures/Cards/WeatherCard.cs
@@ -1,5 +1,6 @@
 ﻿using izolabella.OpenWeatherMap.NET.Classes.Responses;
 using izolabella.OpenWeatherMap.NET.Classes.Responses.CurrentWeatherData;
+using izolabella.OpenWeatherMap.NET.Classes.Responses.OneCall;
 using Mercury.Snapshot.Objects.Structures.UserStructures.Personalization;
 using Mercury.Snapshot.Objects.Util.Managers;
 using System;
@@ -17,23 +18,33 @@ namespace Mercury.Snapshot.Objects.Structures.Cards
         {
             List<EmbedFieldBuilder> Builders = new();
             CultureInfo UserCulture = Profile.Settings.CultureSettings.Culture;
-            WeatherResponse? WeatherToday = await WeatherManager.GetWeatherForToday(Profile.Settings.WeatherSettings.Zip);
             OneCallWeatherResponse? Weather = await WeatherManager.GetWeather(Profile.Settings.WeatherSettings.Zip);
-            if(WeatherToday != null && Weather != null)
+            if(Weather != null)
             {
-                double Temperature = WeatherToday.Main.Temp;
-                double TemperatureMax = WeatherToday.Main.TempMaximum;
-                double TemperatureMin = WeatherToday.Main.TempMinimum;
+                double Temperature = Weather.Current.Temperature;
+                double TemperatureMax = 0;
+                double TemperatureMin = 0;
+                foreach(DailyWeatherData Data in Weather.Daily)
+                {
+                    if(TemperatureMax < Data.Temperature.Max)
+                    {
+                        TemperatureMax = Data.Temperature.Max;
+                    }
+                    if(TemperatureMin > Data.Temperature.Min)
+                    {
+                        TemperatureMin = Data.Temperature.Min;
+                    }
+                }
                 EmbedFieldBuilder Today = new()
                 {
                     Name = $"TODAY'S WEATHER • {DateTime.Today.ToString(UserCulture.DateTimeFormat.LongDatePattern, UserCulture)}",
                 };
                 Today.Value = $"" +
-                    $"{Temperature.ToString(UserCulture)}°C - {WeatherToday.CityName}\n" +
-                    $"H: {TemperatureMax.ToString(UserCulture)}°C L: {TemperatureMin.ToString(UserCulture)}°C\n";
-                foreach(izolabella.OpenWeatherMap.NET.Classes.Responses.OneCall.HourlyWeatherData? Wt in Weather.Hourly)
+                    $"{Temperature.ToString(UserCulture)}°C - {Weather.Current.CityName}\n" +
+                    $"H: {TemperatureMax.ToString(UserCulture)}°C L: {TemperatureMin.ToString(UserCulture)}°C\n\n";
+                foreach(HourlyWeatherData? Wt in Weather.Hourly.Where(Hr => Hr.WeatherTime.ToLocalTime().Date == DateTime.Today))
                 {
-                    Today.Value += $"{Wt.Temperature}°C {Wt.WeatherTime.ToShortTimeString()}";
+                    Today.Value += $"{Wt.WeatherTime.ToLocalTime().ToShortTimeString()}: {Wt.Temperature}°C\n";
                 }
                 Builders.Add(Today);
             }
